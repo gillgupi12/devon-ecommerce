cb1bebdc4d98b0703942207c4589972ad560896c
MoviesAPI-SpringBoot <gillgupi12@gmail.com>
2024-09-28 00:36:53 +0800

    feat: clean up login, register pages

--- demoapplication/src/components/pages/login.tsx
+++ demoapplication/src/components/pages/login.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { TextInput } from '@mantine/core';
 import CustomButton from '../atoms/button';
 import api from '../../api/auth/index'
+import { Container } from '@mantine/core';
+import { Link } from 'react-router-dom';
 
 const LoginPage: React.FC = () => {
 
@@ -19,20 +21,26 @@ const LoginPage: React.FC = () => {
     }
     return (
         <>
-            <form onSubmit={handleSubmit} className='space-y-2'>
-                <TextInput label="Username"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(event) => setUserName(event.target.value)}
-                />
-                <TextInput label="Password"
-                    placeholder="Password"
-                    type='password'
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                />
-                <CustomButton type="submit">Login</CustomButton>
-            </form>
+            <Container size={'xs'} py={35} bg={'white'} className='border rounded'>
+                <p className="text-xl font-semibold pb-4">Log into your Devondemo account</p>
+                <form onSubmit={handleSubmit} className='space-y-3'>
+                    <TextInput label="Username"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(event) => setUserName(event.target.value)}
+                    />
+                    <TextInput label="Password"
+                        placeholder="Password"
+                        type='password'
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                    <div className="w-full flex flex-row justify-between">
+                        <Link to="/forgot-password" className='text-sm underline text-red-500'>Forgot Password?</Link>
+                        <CustomButton type="submit" className='w-1/4' color={'black'} >Login</CustomButton>
+                    </div>
+                </form>
+            </Container>
         </>
     )
 }
--- demoapplication/src/components/pages/register.tsx
+++ demoapplication/src/components/pages/register.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { TextInput } from '@mantine/core';
 import CustomButton from '../atoms/button';
+import { Container } from '@mantine/core';
 import api from '../../api/auth/index'
 
 const RegisterPage: React.FC = () => {
@@ -22,41 +23,47 @@ const RegisterPage: React.FC = () => {
     }
     return (
         <>
-            <form onSubmit={handleSubmit} className='space-y-2 container-sm'>
-                <TextInput label="First name"
-                    placeholder="First name"
-                    type='text'
-                    className='flex-grow'
-                    value={firstName}
-                    onChange={(event) => setFirstname(event.target.value)}
-                />
-                <TextInput label="Last name"
-                    placeholder="Last name"
-                    className='flex-grow'
-                    type='text'
-                    value={lastName}
-                    onChange={(event) => setLastname(event.target.value)}
-                />
+            <Container size={'xs'} py={35} bg={'white'} className='border rounded'>
+                <p className="text-xl font-semibold leading-0">Don't have a devondemo account? Sign Up!</p>
+                <p className="text-sm  font-light pb-4 leading-0">Let's get started! </p>
+                <form onSubmit={handleSubmit} className='space-y-3 '>
+                    <TextInput label="First name"
+                        placeholder="First name"
+                        type='text'
+                        className='flex-grow'
+                        value={firstName}
+                        onChange={(event) => setFirstname(event.target.value)}
+                    />
+                    <TextInput label="Last name"
+                        placeholder="Last name"
+                        className='flex-grow'
+                        type='text'
+                        value={lastName}
+                        onChange={(event) => setLastname(event.target.value)}
+                    />
 
-                <TextInput label="Email"
-                    placeholder="Email"
-                    className='flex-grow'
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                />
-                <TextInput label="Username"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(event) => setUserName(event.target.value)}
-                />
-                <TextInput label="Password"
-                    placeholder="Password"
-                    type='password'
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                />
-                <CustomButton type="submit">Login</CustomButton>
-            </form>
+                    <TextInput label="Email"
+                        placeholder="Email"
+                        className='flex-grow'
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                    <TextInput label="Username"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(event) => setUserName(event.target.value)}
+                    />
+                    <TextInput label="Password"
+                        placeholder="Password"
+                        type='password'
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                    <div className="flex justify-end">
+                        <CustomButton type="submit" color={'black'} >Register</CustomButton>
+                    </div>
+                </form>
+            </Container>
         </>
     )
 }
